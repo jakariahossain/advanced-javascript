66020fd73d96ff72174b411de59ac88823e3b318
jakariahossain <rootcomsp@gmail.com>
2021-02-02 00:42:24 +0600

    break, continue, array-split, closure

---
+++ array-split.js
@@ -0,0 +1,12 @@
+const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2, 8);
+
+
+const removed = nums.splice(2,5,77,88,99);
+
+// console.log(removed);
+// console.log(nums);
+
+const together = nums.join("ami");
+
+console.log(together);
